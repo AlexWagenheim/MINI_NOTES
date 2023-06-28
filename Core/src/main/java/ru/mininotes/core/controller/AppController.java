f40f619b8ed3717e725bc6f55266ff029ac763ef
AlexWagenheim <wagenheim.alex@yandex.ru>
2023-06-28 22:50:04 +0300

    Подключены Thymeleaf и Spring Security. Создан макет главной страницы и страница входа.

--- Core/src/main/java/ru/mininotes/core/controller/AppController.java
+++ Core/src/main/java/ru/mininotes/core/controller/AppController.java
@@ -1,4 +1,19 @@
 package ru.mininotes.core.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AppController {
+
+    @GetMapping(value = "/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping(value = "/login")
+    public String login(Model model) {
+        return "login";
+    }
 }
